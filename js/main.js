58199d06a8263186e9f0ace72721f39f1fa853fa
duyhd0510 <duy.vd19010055@st.phenikaa-uni.edu.vn>
2023-01-31 15:53:10 +0700

    render post list

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-console.log('hello world');
+import axiosClient from "./api/axiosClient";
+import postApi from "./api/postApi";
+
+async function main() {
+  try {
+    const queryParams = {
+      _page: 1,
+      _limit: 5,
+    }
+    const data = await postApi.getAll(queryParams);
+    console.log(data);
+  } catch (error) {
+    console.log('get all failed', error);
+  }
+}
+
+main();
