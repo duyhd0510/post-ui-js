d5ff9cc02c76c419f2a329dda4e26cd456ac84df
duyhd0510 <duy.vd19010055@st.phenikaa-uni.edu.vn>
2023-02-01 15:52:46 +0700

    refactor code

---
+++ js/utils/post.js
@@ -0,0 +1,50 @@
+import { setTextContent, truncateText } from "./common";
+import dayjs from "dayjs";
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+dayjs.extend(relativeTime);
+
+export function createPostElement(post) {
+  if (!post) return;
+
+  // find template and clone template
+  const postTemplate = document.getElementById('postTemplate');
+  if (!postTemplate) return;
+
+  const liElement = postTemplate.content.firstElementChild.cloneNode(true);
+  if (!liElement) return;
+
+  setTextContent(liElement, '[data-id="title"]', post.title);
+  setTextContent(liElement, '[data-id="description"]', truncateText(post.description, 100));
+  setTextContent(liElement, '[data-id="author"]', post.author);
+
+  // calc timespan
+  setTextContent(liElement, '[data-id="timeSpan"]', ` - ${dayjs(post.updatedAt).fromNow()}`);
+
+  const thumbnailElement = liElement.querySelector('[data-id="thumbnail"]');
+  if (thumbnailElement) {
+    thumbnailElement.src = post.imageUrl;
+
+    thumbnailElement.addEventListener('error', () => {
+      thumbnailElement.src = 'https://via.placeholder.com/1368x400?text=image'
+    })
+  }
+
+  // attach events
+
+  return liElement;
+}
+
+export function renderPostList(elementId, postList) {
+  if (!Array.isArray(postList)) return;
+
+  const ulElement = document.getElementById(elementId);
+  if (!ulElement) return;
+
+  ulElement.textContent = '';
+
+  postList.forEach((post) => {
+    const liElement = createPostElement(post);
+    ulElement.appendChild(liElement);
+  })
+}
